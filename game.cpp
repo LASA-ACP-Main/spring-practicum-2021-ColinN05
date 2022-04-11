1805c63e9cf3bd5fc5c64dc137f3be030acfa67d
denezrg <35906181+denezrg@users.noreply.github.com>
2022-04-11 09:31:52 -0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -3,41 +3,72 @@
 
 CheckersBoard::CheckersBoard()
 {
-    // set entire board to be empty
-    for (int i = 0; i < 8; i++)
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            Squares[i][j] = 0;
-        }
-    }
+  // set entire board to be empty
+  for (int i = 0; i < 8; i++)
+  {
+      for (int j = 0; j < 8; j++)
+      {
+          Squares[i][j] = Color::none;
+      }
+  }
+  //Then make the rows contain their proper starting pieces
+  
 }
 
 void CheckersBoard::Display()
 {
-    for (int j = 0; j < 8; j++)
+  std::string board_text = "";
+  for (int j = 0; j < 8; j++)
+  {
+    for (int i = 0; i < 8; i++)
     {
-        for (int i = 0; i < 8; i++)
+      // just print square number for now, might change to special character
+      switch (Squares[i][j])
+      {
+        case Color::none:
         {
-            // just print square number for now, might change to special character
-            std::cout << Squares[i][j] << " ";
+          board_text += "  ";
+          break;
         }
-        std::cout << '\n';
+        case Color::black:
+        {
+          board_text += "⚫ ";
+          break;
+        }
+        case Color::white:
+        {
+          board_text += "⚪ ";
+          break;
+        }
     }
+    board_text += '\n';
+  }
+  std::cout << board_text;
 }
 
 bool CheckersBoard::TryMove(int x0, int y0, int x1, int y1)
 {
-    // most of the game logic is here
-    //...
+    Color colorstart = Squares[x0][y0];
+    Color colortry = Squares[x1][y1];
+    if (Squares[x0][y0]){
+      
+    }
     return true; // just return true for now
 }
 
 CheckersGame::CheckersGame()
-{
-    // put pieces in starting position
+{    
 }
 
 void CheckersGame::Play()
 {
+  int winningPlayer = 0;
+  while (winningPlayer == 0)
+  {
+    // Get move input from player
+    std::cout << "Move >";
+    std::string playerInput;
+    std::cin >> playerInput;
+    
+  }
 }
