485d5babd39e87af077970cf8b80de8f3078424c
denezrg <35906181+denezrg@users.noreply.github.com>
2022-04-11 09:32:16 -0500

    Update game.h

--- game.h
+++ game.h
@@ -1,20 +1,26 @@
+enum class Color
+{
+  none = 0, black = 1, white = 2
+};
+
 class CheckersBoard
 {
 public:
 	CheckersBoard();
 
-    void Display();
+  void Display();
 
-    // tries to move piece at (x0,y0) to (x1,y1) returns true if move could be made and false if not
-    bool TryMove(int x0, int y0, int x1, int y1); 
+  // tries to move piece at (x0,y0) to (x1,y1) returns true if move could be made and false if not
+  bool TryMove(int x0, int y0, int x1, int y1); 
 private:
-	int Squares[8][8]; // 0 = empty square, 1 = black square, 2 = white square
+	Color Squares[8][8]; // 0 = empty square, 1 = black square, 2 = white square
 };
 
 class CheckersGame
 {
 public:
-    void Play();
+  CheckersGame();
+  void Play();
 private:
-    CheckersBoard Board; 
+  CheckersBoard Board; 
 };
