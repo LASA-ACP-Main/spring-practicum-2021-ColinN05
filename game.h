ec9db8e2580f246d0f846da72cad68de94873e01
denezrg <35906181+denezrg@users.noreply.github.com>
2022-04-27 09:17:47 -0500

    game.h function upkeep

--- game.h
+++ game.h
@@ -1,6 +1,6 @@
 enum class Color
 {
-  none = 0, black = 1, white = 2
+  none = 0, black = 1, white = 2 // human player is always white
 };
 
 class CheckersBoard
@@ -11,7 +11,8 @@ public:
   void Display();
 
   // tries to move piece at (x0,y0) to (x1,y1) returns true if move could be made and false if not
-  bool TryMove(int x0, int y0, int x1, int y1); 
+  bool TryMove(int x0, int y0, int x1, int y1, Color moverc);
+  void BotMove();
 private:
 	Color Squares[8][8]; // 0 = empty square, 1 = black square, 2 = white square
 };
@@ -21,6 +22,7 @@ class CheckersGame
 public:
   CheckersGame();
   void Play();
+  void PlayerMakeMove();
 private:
   CheckersBoard Board; 
 };
