5f660fefb524cf390b97f474d927b4500f20e355
Colin Neaville <35436544+ColinN05@users.noreply.github.com>
2022-05-19 20:11:41 -0500

    Update GameTree.h

--- GameTree.h
+++ GameTree.h
@@ -6,20 +6,21 @@ struct GameNode
 {
 public:
   GameNode(CheckersBoard* board);
+  ~GameNode();
+  void Free(int depth = 0);
   CheckersBoard* Board;
   std::vector<GameNode*> Branches;
   //int MinimaxScore(Color );
   void FillTree(CheckersBoard* startBoard, Color startColor,int depth = 0);
-};
+  float EvaluateScore();
 
-class GameTree
-{
-public:
-  GameTree();
-  static int maxdepth = 5;
+  const static int maxdepth = 4;
   GameNode* ptop;
-  int MiniMax(CheckersBoard board, int depth, bool isMaxPlayer);
-  bool isTerminal(CheckersBoard board);
-private:
+  float MiniMax(GameNode* node, int depth, Color player, float alpha = -999.0f, float beta = 999.0f);
+  Color isTerminal(CheckersBoard* board); // returns winning player color
 
+  GameNode* GetBestMove();
+  GameNode* BestPotentialMove(GameNode* node, int depth, Color player);
+  GameNode* BestAverageMove(GameNode* node, int depth, Color player);
+  float AverageScore(GameNode* node, int depth);
 };
